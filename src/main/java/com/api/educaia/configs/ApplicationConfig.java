56840d4bb62fc97826c6b9032b6a8e7c5490b74f
AndersonLLeite <anderson.tom.leite@gmail.com>
2024-04-22 23:44:41 -0300

    Adicionado o Dockerfile para o deploy no render

--- src/main/java/com/api/educaia/configs/ApplicationConfig.java
+++ src/main/java/com/api/educaia/configs/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.api.educaia.configs;
 
 import com.api.educaia.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,7 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
-
+    @Autowired
     private final UserRepository repository;
 
     @Bean
